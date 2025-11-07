d3974c3de409d6c161095285e0522f5c8b52e3bd
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2025-11-07 23:11:12 +0100

    Real work

---
+++ python/warmcat.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import subprocess
+import signal
+import time
+
+def start_shell_subprocess():
+    # Start the shell subprocess
+    shell_process = subprocess.Popen(['glxgears'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Suspend and unsuspend the shell process every second
+    while True:
+        # Suspend the shell process
+        shell_process.send_signal(signal.SIGTSTP)
+
+        # Wait for 1 second
+        time.sleep(4)
+
+        # Resume the shell process
+        shell_process.send_signal(signal.SIGCONT)
+
+        # Wait for 1 second
+        time.sleep(2)
+
+start_shell_subprocess()
+
+def main():
+    start_shell_subprocess()
+
+if __name__ == '__main__':
+    main()
